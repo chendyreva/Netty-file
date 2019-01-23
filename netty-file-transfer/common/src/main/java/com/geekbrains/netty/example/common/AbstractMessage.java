0162380328ad96d04ce6941f048914e437c588c3
chendyreva <nat-217@mail.ru>
2019-01-23 17:54:47 +0500

    Netty-file

---
+++ netty-file-transfer/common/src/main/java/com/geekbrains/netty/example/common/AbstractMessage.java
@@ -0,0 +1,7 @@
+package com.geekbrains.netty.example.common;
+
+import java.io.Serializable;
+
+public abstract class AbstractMessage implements Serializable {
+}
+
---
+++ netty-file-transfer/common/src/main/java/com/geekbrains/netty/example/common/FileRequest.java
@@ -0,0 +1,14 @@
+package com.geekbrains.netty.example.common;
+
+public class FileRequest extends AbstractMessage {
+    private String filename;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public FileRequest(String filename) {
+        this.filename = filename;
+    }
+}
+
